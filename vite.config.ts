a0684a7392f2b824af879b6b52bb9504c69490f9
Vinícius Cezar <127261692+ViCezar@users.noreply.github.com>
2025-06-03 21:58:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio-redesign/', // 👉 nome do seu repositório aqui
   plugins: [react()],
 })
